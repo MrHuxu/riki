4726691e03488ede68eb4a12e9edb933dd0210b8
xhu <hxtheone@gmail.com>
2016-03-10 07:58:04 -0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+class riki extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Me </Text>
+        <Image style={{ width: 63, height: 81 }} source={ {uri: 'https://raw.githubusercontent.com/MrHuxu/img-repo/master/blog/1.pic.jpg' }} />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('riki', () => riki);
